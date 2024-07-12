6453a19409c0dedf1cb33be4764d442b5409e374
nikhil991gupta <nikhil991gupta@gmail.com>
2024-07-12 16:21:18 +0530

    adding_signup_action

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -9,9 +9,10 @@ export async function POST(request:Request) {
     try {
         //always waait
        const {email,username,password} =await  request.json()
+       // to check 
        const existinguserVerificationByUsername=await UserModel.findOne(
         {
-        username,
+        username,// find user name if it is already verified by username
         isVerified:true
     } )
     if(existinguserVerificationByUsername)
@@ -28,24 +29,58 @@ export async function POST(request:Request) {
             })
         }
        const existinguserbyemail=await UserModel.findOne({email})
+       const verifyCode=Math.floor(100000+Math.random() * 900000).toString();
        if( existinguserbyemail)
-        {
+        { 
             return true;
         }
         else{
-            //existing user by mail not found sor register
+            // entered else means first time user
+            //existing user by mail not found so  register
             const hashedpasswrod=await bcrypt.hash(password,10)
             // object rederence pount so the value can change
             const expiryDate=new Date()
-            expiryDate.setDate(expiryDate.getDate()+1)
-            new UserModel({
+            expiryDate.setHours(expiryDate.getHours()+1)
+        const newUser= new UserModel({
                 username,
                 email,
                 password:hashedpasswrod,
+                verifyCode,
+                expiryDate,    
+                isVerified:false,
+                isAcceptingmessage:true,
+                messages:[],
                 
             })
+            await newUser.save()
 
         }  
+
+    //Send verificaion email
+
+const emailresponse=await sendVerificationEmail(
+    email,
+    username,
+    verifyCode,
+)
+if(!emailresponse.success)
+{
+   return Response.json({
+    success:false,
+    message:`Email not sent ${ emailresponse.message}`,
+   },
+    {
+        status:500
+
+    }
+   )
+}
+return Response.json({
+    success:true,
+    message:"Registered Sucessfully.Kinly Verify Your Email", 
+   },{
+    status:200
+   })
     } catch (error) {
         console.log("object",error)
         return Response.json(
